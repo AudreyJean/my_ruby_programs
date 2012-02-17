326a2068e6e678591ddcc1ebd9916abc0bd9635e
AudreyJean <Fiberfolly@Laptop.(none)>
2012-02-16 19:13:00 -0800

    Cal_len.rb

---
+++ cal_len.rb
@@ -0,0 +1,13 @@
+multiplier = ''
+actual = ''
+divisor = '4.0'
+while multiplier != '0' 
+  print "What multiplier do you want to use (type '0' to quit)? "
+  multiplier = gets.chomp()
+  while actual != '0'
+    print "What is the actual measurement (type '0' to quit)? "
+    actual = gets.chomp()
+    length = actual.to_f / divisor.to_f * multiplier.to_f * 0.01
+    puts length.to_s
+  end
+end
