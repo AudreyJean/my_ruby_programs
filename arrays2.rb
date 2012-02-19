a3d249bcadbc4eae771ee990258b8dd465276d3b
AudreyJean <Fiberfolly@Laptop.(none)>
2012-02-19 11:43:13 -0800

    my sorted array program working

--- arrays2.rb
+++ arrays2.rb
@@ -3,34 +3,31 @@ j = 0
 words = []
 words2 = []
 a_word = ' '
-b=word = ''
 i = 0
 puts "Type a word and press <Enter>:"
-# Loop to accept an unspecified number of words-- creates original array
+#~ # Loop to accept an unspecified number of words-- creates original array
 while a_word != ""
   a_word = gets.chomp
   if a_word != ""
     words.push a_word
     i = i + 1
-  end
-end
+  end # end if
+end # end while
 
-i = words.length
-until  i == words2.length
+while words.length > 0
+  i = words.length - 1
   # I will sort by looking for the lowest value word in the original array and pushing it on to the new array
-  # i = words.length
-  a_word = words[0] # initialize with the first element in the array
-  a_index = 0
-  for j in 1..i
-    puts "debug line " + a_word
-    puts "debug line " + words[j]
-    if a_word > words[j]  # compare the two words
-      a_word = words[j]
-      a_index = j
-    end  # end if
-  end  # end for
-  words2.push a_word  # push smallest word onto new array
-  words.delete_at(a_index) # delete the lowest element in the original array
-end
-puts ""
+    a_word = words[0] # initialize with the first element in the array
+    a_index = 0
+    for j in 1..i 
+      b_word = words[j] # assign  second element to a var	    
+      if a_word.upcase > b_word.upcase  # compare the two words
+        a_word = words[j]
+        a_index = j  # variable so I can reference the words pushed onto the new array
+      end  # end if
+    end  # end for
+    words2.push a_word  # push smallest word onto new array
+    words.delete_at(a_index) # delete the lowest element in the original array
+    i = words.length - 1
+end # end while
 puts words2
